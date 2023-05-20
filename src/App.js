882ce9c07c078c9dd32d8e3e6d3374c020f52671
Samridhi <samridhi231710@gmail.com>
2023-05-20 15:13:55 +0530

    Add Media query for backgroung

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+// import HeroImage from "./components/HeroImage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
+    <div className="bg-img">
+      <img src='/images/hero-img-mount.png' alt='mount' className="mount"></img>
+      <img src='/images/hero-img-tower.png' alt='tower' className="tower"></img>
       <Navbar />
-    </>
-    
+    </div>
   );
 }
 
