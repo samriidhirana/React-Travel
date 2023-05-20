790f70cb9cf56883e08813c65bbad3eae9a715ca
Samridhi <samridhi231710@gmail.com>
2023-05-20 23:40:52 +0530

    Add banner text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,13 +18,18 @@ const Home = () => {
           className="tower"
         ></img>
         <Navbar />
+        <div className="hero-text">
+          <h1>Explore</h1>
+          <p>日本 • にほん</p>
+          <h2>japan</h2>
+        </div>
       </div>
       <section className="destination-section">
-      <div className="">
-        <h1>Popular Destinations</h1>
-        <h2>Image carsole</h2>
-      </div>
-    </section>
+        <div className="">
+          <h1>Popular Destinations</h1>
+          <h2>Image carsole</h2>
+        </div>
+      </section>
     </>
   );
 };
