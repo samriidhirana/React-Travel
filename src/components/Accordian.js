ae50c8cb501c1c4713dba7ec0a80cd4ded95b0d0
Samridhi Rana <samridhi@Samridhis-MacBook-Air.local>
2023-06-16 12:56:00 +0530

    Add faq section

---
+++ src/components/Accordian.js
@@ -0,0 +1,93 @@
+import React from "react";
+import Accordion from "react-bootstrap/Accordion";
+
+import "./Accordian.css";
+
+const Accordian = () => {
+  return (
+    <div>
+      <Accordion defaultActiveKey="0" flush>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>
+            <h2 className="acc-heading">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </h2>
+          </Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header className="acc-heading">
+            <h2 className="acc-heading">
+              Lorem ipsum dolor sit amet, consectetur
+            </h2>
+          </Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="2">
+          <Accordion.Header className="acc-heading">
+            <h2 className="acc-heading">Lorem ipsum dolor sit amet.</h2>
+          </Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="3">
+          <Accordion.Header className="acc-heading">
+            <h2 className="acc-heading">
+              Lorem ipsum dolor sit amet, consectetur adipisicing.
+            </h2>
+          </Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="4">
+          <Accordion.Header className="acc-heading">
+            <h2 className="acc-heading">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </h2>
+          </Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </div>
+  );
+};
+
+export default Accordian;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,26 +4,41 @@ import Footer from "../components/Footer";
 
 import "./Contact.css";
 import Form from "../components/Form";
+import Accordian from "../components/Accordian";
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
-      <div className="contact-section">
-        <div className="contact-img">
+      <div className="row">
+        <div className="contact-img col-7">
           <div className="overlay">
-            <h1 className="contact-title">Dont know where to start?</h1>
-            <p>We are here to help you out!</p>
+            <h1
+              className="contact-title"
+              data-aos="fade-right"
+              data-aos-duration="800"
+            >
+              Dont know where to start?
+            </h1>
+            <p data-aos="fade-right" data-aos-duration="800">
+              We are here to help you out!
+            </p>
           </div>
         </div>
-        <div className="contact-form">
-          <h1>Get in Touch</h1>
-          <div className="form-area">
+        <div className="contact-form col-5">
+          <h1 data-aos="fade-left" data-aos-duration="800">
+            Get in Touch
+          </h1>
+          <div
+            className="form-area"
+            data-aos="fade-left"
+            data-aos-duration="800"
+          >
             <Form />
           </div>
         </div>
       </div>
-      <div className="contact-map">
+      <div className="contact-map" data-aos="zoom-in" data-aos-duration="600">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3503.3230140244473!2d77.09288301555628!3d28.59008499275066!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1b5dbed6ebd9%3A0xddb7cb3ece1236d!2sTruewebsolution!5e0!3m2!1sen!2sin!4v1679040681372!5m2!1sen!2sin"
           width="100%"
@@ -34,7 +49,17 @@ const Contact = () => {
           referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
       </div>
-      <div className="contact-ques-section"></div>
+      <div className="contact-faq" data-aos="fade-up" data-aos-duration="1000">
+        <div className="row">
+          <div className="col-6 faq-heading">
+            <h2>FAQ</h2>
+            <h1>Frequently asked questions.</h1>
+          </div>
+          <div className="col-6 faq-section">
+            <Accordian />
+          </div>
+        </div>
+      </div>
 
       <Footer />
     </div>
