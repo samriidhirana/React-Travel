0bbd9872c8fc3765a4d40c83a210b2ebbe3423f2
Samridhi Rana <samridhi@Samridhis-MacBook-Air.local>
2023-06-16 14:09:08 +0530

    Configure React app for deployment to GitHub Pages

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -8,24 +8,11 @@ import { useFavContext } from "../context/fav-context";
 
 const ImageCard = () => {
   const { filteredArray } = useFavContext();
-
-  // const { title } = filteredArray;
-
-  // const navigate = useNavigate();
-
-  // const navigateToSinglePage = () => {
-  //   console.log(title);
-  //   // navigate(`/destinations/${title}`);
-  // };
   return (
     <div className="img-grid" data-aos="fade-up" data-aos-duration="1000">
       {filteredArray.map((curElem, index) => {
         if (curElem.popular) {
-          return (
-            <div className="img-card" key={index}>
-              <Card {...curElem} />
-            </div>
-          );
+          return <Card key={index} {...curElem} />;
         }
       })}
     </div>
@@ -33,29 +20,3 @@ const ImageCard = () => {
 };
 
 export default ImageCard;
-
-{
-  /* <div
-              className="img-card"
-              style={{ backgroundImage: `url('${curElem.url}')` }}
-              key={index}
-              onClick={navigateToSinglePage}
-            >
-              <FontAwesomeIcon
-                icon={faHeart}
-                className={
-                  curElem.favorite ? "card-heart-icon-color" : "card-heart-icon"
-                }
-                // onClick={() => onFavIconClickHandler(id)}
-                onClick={(event) => {
-                  event.stopPropagation();
-                  // console.log("icon clicked");
-                  onFavIconClickHandler(curElem.id);
-                }}
-              />
-              <div className="image-info">
-                <h2>{curElem.title}</h2>
-                <p>{curElem.description}</p>
-              </div>
-            </div> */
-}
