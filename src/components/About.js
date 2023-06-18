8584268c37c225f4c1c02bf63576f24e0dce4b50
Samridhi Rana <samridhi@Samridhis-MacBook-Air.local>
2023-06-18 20:01:39 +0530

    Restructure Footer

--- src/components/About.js
+++ src/components/About.js
@@ -3,11 +3,15 @@ import "./About.css";
 
 const About = () => {
   return (
-    <div className="about-bg">
-      <h1 className="about-title" data-aos="fade-right" data-aos-duration="700">
-        About Us
-      </h1>
-      <div className="about-flex">
+    <div className="about-bg row">
+      <div className="col-lg-6 col-md-6 col-sm-12">
+        <h1
+          className="about-title"
+          data-aos="fade-right"
+          data-aos-duration="700"
+        >
+          About Us
+        </h1>
         <div data-aos="fade-right" data-aos-duration="700">
           <p className="about-info">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste
@@ -20,22 +24,23 @@ const About = () => {
             voluptatibus.
           </p>
         </div>
-        <div
-          className="about-images-scetion"
-          data-aos="fade-left"
-          data-aos-duration="700"
-        >
-          <img
-            src="/images/about/about-img1Medium.jpeg"
-            alt="about-img1"
-            className="about-img1"
-          ></img>
-          <img
-            src="/images/about/about-img2Medium.jpeg"
-            alt="about-img2"
-            className="about-img2"
-          ></img>
-        </div>
+      </div>
+
+      <div
+        className="about-images-scetion col-lg-6 col-md-6 col-sm-12"
+        data-aos="fade-left"
+        data-aos-duration="700"
+      >
+        <img
+          src="/images/about/about-img1Medium.jpeg"
+          alt="about-img1"
+          className="about-img1"
+        ></img>
+        <img
+          src="/images/about/about-img2Medium.jpeg"
+          alt="about-img2"
+          className="about-img2"
+        ></img>
       </div>
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
 
 import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className="footer-bg">
-      <div className="footer-info-section">
-        <NavLink to="/" className="underline">
+    <div className="footer-bg row">
+      <div className="footer-info-section col-2">
+        <Link to="/" className="underline">
           <div className="logo">
             <img
               src="/images/sun.png"
@@ -18,40 +19,51 @@ const Footer = () => {
 
             <span className="footer-brand-icon">Ryoko</span>
           </div>
-        </NavLink>
+        </Link>
       </div>
-      <div className="footer-links-section">
+      <div className="footer-links-section col-4">
         <div className="footer-main-links">
-          <NavLink
+          <Link
             to="/destinations"
             className={({ isActive }) =>
               isActive ? "underline active" : "underline"
             }
           >
             <span className="footer-navi-link">Destination</span>
-          </NavLink>
-          <NavLink
+          </Link>
+          <Link
             to="/activities"
             className={({ isActive }) =>
               isActive ? "underline active" : "underline"
             }
           >
             <span className="footer-navi-link">Activities</span>
-          </NavLink>
-          <NavLink
+          </Link>
+          <Link
             to="/contact-us"
             className={({ isActive }) =>
               isActive ? "underline active" : "underline"
             }
           >
             <span className="footer-navi-link">Contact</span>
-          </NavLink>
+          </Link>
         </div>
         <p className="footer-about-rights">All rights reseverd</p>
       </div>
-      <div>
-        {/* <FontAwesomeIcon icon="fa-brands fa-twitter" /> */}
-        {/* <FontAwesomeIcon icon="fa-brands fa-twitter" /> */}
+      <div className="col-3"></div>
+      <div className="col-3 social-media-icons">
+        <div className="social-icon">
+          <FontAwesomeIcon icon={faHeart} className="heart-icon" />
+        </div>
+        <div className="social-icon">
+          <FontAwesomeIcon icon={faHeart} className="heart-icon" />
+        </div>
+        <div className="social-icon">
+          <FontAwesomeIcon icon={faHeart} className="heart-icon" />
+        </div>
+        <div className="social-icon">
+          <FontAwesomeIcon icon={faHeart} className="heart-icon" />
+        </div>
       </div>
     </div>
   );
